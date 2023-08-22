KozhNat/Autotests

Make test example
e2e example with text field and manual widget manipulation

examples/profile/integration_test/gherkin_suite_test.g.dart
@@ -1,0 +1,328 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gherkin_suite_test.dart';
+
+// **************************************************************************
+// GherkinSuiteTestGenerator
+// **************************************************************************
+
+class _CustomGherkinIntegrationTestRunner extends GherkinIntegrationTestRunner {
+  _CustomGherkinIntegrationTestRunner({
+    required FlutterTestConfiguration configuration,
+    required StartAppFn appMainFunction,
+    required Timeout scenarioExecutionTimeout,
+    AppLifecyclePumpHandlerFn? appLifecyclePumpHandler,
+    LiveTestWidgetsFlutterBindingFramePolicy? framePolicy,
+  }) : super(
+          configuration: configuration,
+          appMainFunction: appMainFunction,
+          scenarioExecutionTimeout: scenarioExecutionTimeout,
+          appLifecyclePumpHandler: appLifecyclePumpHandler,
+          framePolicy: framePolicy,
+        );
+
+  @override
+  void onRun() {
+    testFeature0();
+  }
+
+  void testFeature0() {
+    runFeature(
+      name: 'Профиль:',
+      tags: <String>[],
+      run: () {
+        runScenario(
+          name: 'Успешное заполнение профиля',
+          description: null,
+          path:
+              '/Users/voronin/flutter-elementary-test/examples/profile/./integration_test/features/profile.feature',
+          tags: <String>[],
+          steps: [
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу к редактированию профиля',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю фамилию "Voronin"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю дату рождения "1998-12-31"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю имя "Vladislav"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю отчество "Vladimirovich"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я выбираю город "Voronezh"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я выбираю "Sleep" из интересов',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я заполняю заметку о себе "Тестовая заметка о себе"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу к редактированию профиля',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу заполненные поля ФИО',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я вижу заполненное поле даты рождения',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу заполненное поле города',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу выбранные интересы',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу заполненное поле заметки о себе',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+          ],
+          onBefore: () async => onBeforeRunFeature(
+            name: 'Профиль',
+            path:
+                r'/Users/voronin/flutter-elementary-test/examples/profile/./integration_test/features/profile.feature',
+            description: null,
+            tags: <String>[],
+          ),
+          onAfter: () async => onAfterRunFeature(
+            name: 'Профиль',
+            path:
+                r'/Users/voronin/flutter-elementary-test/examples/profile/./integration_test/features/profile.feature',
+            description: null,
+            tags: <String>[],
+          ),
+        );
+      },
+    );
+  }
+}
+
+void executeTestSuite({
+  required FlutterTestConfiguration configuration,
+  required StartAppFn appMainFunction,
+  Timeout scenarioExecutionTimeout = const Timeout(const Duration(minutes: 10)),
+  AppLifecyclePumpHandlerFn? appLifecyclePumpHandler,
+  LiveTestWidgetsFlutterBindingFramePolicy? framePolicy,
+}) {
+  _CustomGherkinIntegrationTestRunner(
+    configuration: configuration,
+    appMainFunction: appMainFunction,
+    appLifecyclePumpHandler: appLifecyclePumpHandler,
+    scenarioExecutionTimeout: scenarioExecutionTimeout,
+    framePolicy: framePolicy,
+  ).run();
+}

surfstudio/qa-school-autotests

examples/profile/integration_test/gherkin_suite_test.g.dart
@@ -1,0 +1,328 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gherkin_suite_test.dart';
+
+// **************************************************************************
+// GherkinSuiteTestGenerator
+// **************************************************************************
+
+class _CustomGherkinIntegrationTestRunner extends GherkinIntegrationTestRunner {
+  _CustomGherkinIntegrationTestRunner({
+    required FlutterTestConfiguration configuration,
+    required StartAppFn appMainFunction,
+    required Timeout scenarioExecutionTimeout,
+    AppLifecyclePumpHandlerFn? appLifecyclePumpHandler,
+    LiveTestWidgetsFlutterBindingFramePolicy? framePolicy,
+  }) : super(
+          configuration: configuration,
+          appMainFunction: appMainFunction,
+          scenarioExecutionTimeout: scenarioExecutionTimeout,
+          appLifecyclePumpHandler: appLifecyclePumpHandler,
+          framePolicy: framePolicy,
+        );
+
+  @override
+  void onRun() {
+    testFeature0();
+  }
+
+  void testFeature0() {
+    runFeature(
+      name: 'Профиль:',
+      tags: <String>[],
+      run: () {
+        runScenario(
+          name: 'Успешное заполнение профиля',
+          description: null,
+          path:
+              '/Users/voronin/flutter-elementary-test/examples/profile/./integration_test/features/profile.feature',
+          tags: <String>[],
+          steps: [
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу к редактированию профиля',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю фамилию "Semenikhina"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю дату рождения "1998-11-14"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю имя "Daria"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я указываю отчество "Evgenevna"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я выбираю город "Voronezh"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я выбираю "Sleep" из интересов',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я заполняю заметку о себе "Тестовая заметка о себе"',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я перехожу к редактированию профиля',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу заполненные поля ФИО',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'И Я вижу заполненное поле даты рождения',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу заполненное поле города',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу выбранные интересы',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Когда Я перехожу далее',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+            (
+              TestDependencies dependencies,
+              bool skip,
+            ) async {
+              return await runStep(
+                name: 'Тогда Я вижу заполненное поле заметки о себе',
+                multiLineStrings: <String>[],
+                table: null,
+                dependencies: dependencies,
+                skip: skip,
+              );
+            },
+          ],
+          onBefore: () async => onBeforeRunFeature(
+            name: 'Профиль',
+            path:
+                r'/Users/voronin/flutter-elementary-test/examples/profile/./integration_test/features/profile.feature',
+            description: null,
+            tags: <String>[],
+          ),
+          onAfter: () async => onAfterRunFeature(
+            name: 'Профиль',
+            path:
+                r'/Users/voronin/flutter-elementary-test/examples/profile/./integration_test/features/profile.feature',
+            description: null,
+            tags: <String>[],
+          ),
+        );
+      },
+    );
+  }
+}
+
+void executeTestSuite({
+  required FlutterTestConfiguration configuration,
+  required StartAppFn appMainFunction,
+  Timeout scenarioExecutionTimeout = const Timeout(const Duration(minutes: 10)),
+  AppLifecyclePumpHandlerFn? appLifecyclePumpHandler,
+  LiveTestWidgetsFlutterBindingFramePolicy? framePolicy,
+}) {
+  _CustomGherkinIntegrationTestRunner(
+    configuration: configuration,
+    appMainFunction: appMainFunction,
+    appLifecyclePumpHandler: appLifecyclePumpHandler,
+    scenarioExecutionTimeout: scenarioExecutionTimeout,
+    framePolicy: framePolicy,
+  ).run();
+}